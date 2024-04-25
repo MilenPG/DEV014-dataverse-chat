c1134dbbd4e40cd8c8f5690f6b220c7cb45bb597
Milen <millalen.pg@gmail.com>
2024-04-25 13:53:28 -0400

    Creación rama para integración de API OpenAI

---
+++ src/lib/openAIApi.js
@@ -0,0 +1,7 @@
+import { getApiKey } from './apiKey.js';
+/*Esta función debe recibir un parámetro que almacena los mensajes; ten presente que dichos mensajes deben 
+seguir una estructura específica, la cual puedes comprender mejor consultando la documentación proporcionada por OpenAI.*/
+
+export const communicateWithOpenAI = (messages) => {
+//Aquí es donde debes implementar la petición con fetch o axios
+};
