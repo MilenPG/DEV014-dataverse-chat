b5042c9ea2e36a0fa77330e9ac2102dffea72334
Milen <millalen.pg@gmail.com>
2024-05-03 14:56:00 -0400

    prueba config AI

---
+++ src/lib/openAIAPI.js
@@ -0,0 +1,10 @@
+//Aquí elaborar la lógica relativa a la IA.
+// Importa la función para obtener la API KEY desde apiKey.js
+import { getApiKey } from './apiKey.js';
+
+export const communicateWithOpenAI = async (messages) => { 
+//Aquí es donde debes implementar la petición con fetch o axios
+const apiKey = ''; //variable provisoria q luego será igual a getApiKey.
+
+
+};
