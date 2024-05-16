a19ccf6164bcb7474f6ed110061cf7f81fc1c7a4
Milen <millalen.pg@gmail.com>
2024-05-16 16:17:34 -0400

    Cambio error-img por la misma pero sin fondo

--- src/views/Error.js
+++ src/views/Error.js
@@ -2,7 +2,7 @@ const Error = () => {
   const viewEl = document.createElement("div");
   const errorPage = `
   <div class='error-page'>
-    <img src='https://ih1.redbubble.net/image.5062941254.9931/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg' alt='Potclean' class='error-img'>
+    <img src='https://github.com/MilenPG/DEV014-dataverse-chat/blob/final-details/src/assets/knuckle-creature.transp.png?raw=true' alt='Potclean' class='error-img'>
       <h1 class='error-h1'>¡Error!</h1>
       <p>Para regresar a la página de inicio, haz click en el banner</p>
   </div>
